a36ab450be3fbc26e849f6b32a9a8b8ed5d3db44
Jeyar Lin <jeyarLin@163.com>
2022-08-23 18:42:56 +0800

    工具函数说明添加

--- lodashTest.js
+++ lodashTest.js
@@ -1,4 +1,6 @@
 /** Used to compose bitmasks for cloning. */
+// https://juejin.cn/post/6844903999972474887
+// ./js/deepClone/deepClone4.js
 const CLONE_DEEP_FLAG_ = 1;
 const CLONE_FLAT_FLAG_ = 2;
 const CLONE_SYMBOLS_FLAG_ = 4;
@@ -10,6 +12,7 @@ function cloneDeep(value) {
 console.log(deep[0] === objects[0]);
 console.log(objects, deep);
 
+// ./js/other/lodash.min.js
 // var objects = [{ a: 1 }, { b: 2 }];
 // var deep = _.cloneDeep(objects);
 // console.log(deep[0] === objects[0]);
