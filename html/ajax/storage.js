0527433ba99369516336352d64ab08642b26bf28
Jeyar Lin <jeyarLin@163.com>
2022-09-16 17:11:21 +0800

    html

---
+++ html/ajax/storage.js
@@ -0,0 +1,15 @@
+const storage = window.localStorage;
+
+const set = (key, value) => {
+  storage.setItem(key, JSON.stringify(value));
+};
+const get = (key) => {
+  return JSON.parse(storage.getItem(key));
+};
+const remove = (key) => {
+  storage.removeItem(key);
+};
+const clear = () => {
+  storage.clear();
+};
+export { set, get, remove, clear };
